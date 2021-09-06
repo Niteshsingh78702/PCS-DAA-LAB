e5c5df3054ec72f15663f767ff60732f58b80985
Niteshsingh78702 <87358716+Niteshsingh78702@users.noreply.github.com>
2021-09-06 23:34:18 +0530

    Add files via upload

---
+++ program-02.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+void finding_index(int n,int arr[])
+{   int st=0;
+    for(int i=0;i<n-1;i++)
+    {
+        for(int j=i+1;j<n-1;j++)
+        {
+          st=arr[i]+arr[j];
+
+        int s=0;
+        int en=n-1;
+        while(s<=en)
+        {
+            int mid=(s+en)/2;
+            if(arr[mid]==st)
+            {
+                cout<<i+1<<" "<<j+1<<" "<<mid+1<<endl;
+                break;
+            }
+            else if(arr[mid]>st)
+            {
+                en=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+
+
+        }
+    }
+}
+int main()
+{
+    int T;
+    cin>>T;
+    while(T>0)
+    {
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>arr[i];
+        }
+       finding_index(n,arr);
+        T--;
+
+    }
+    return 0;
+}
+
+
---
+++ program_03.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+int Total_pair(int n,int arr[],int key)
+{    int c=0;
+    sort(arr,arr+(n));
+    int l=0;
+    int r=0;
+    while(r<n)
+    {
+        if(arr[r]-arr[l]==key)
+        {
+            c++;
+            l++;
+            r++;
+        }
+        else if(arr[r]-arr[l]>key)
+        {
+            l++;
+        }
+        else{
+            r++;
+        }
+    }
+    return c;
+}
+int main()
+{
+    int T;
+    cin>>T;
+    while(T>0)
+    {
+      int n;
+      cin>>n;
+      int arr[n];
+      for(int i=0;i<n;i++)
+      {
+          cin>>arr[i];
+      }
+      int key;
+      cin>>key;
+      cout<<Total_pair(n,arr,key);
+      T--;
+    }
+}
